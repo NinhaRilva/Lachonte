7cccf8620f13d767bfda556ecd6900bd1ed644ab
Adriana R Silva <adrianarafael2010@gmail.com>
2023-05-01 02:44:49 -0300

    entrega ajust

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const galeriaImg = document.querySelectorAll
+  (".galeria li img");
+
+
+function trocarImg(evento) {
+  const imgPrincipal = document.querySelector(".img-principal-galeria img");
+  const pegarImg = evento.currentTarget;
+  imgPrincipal.src = pegarImg.src;
+  imgPrincipal.alt = pegarImg.alt;
+ }
+function clicarIamgem(imagem) {
+  
+  imagem.addEventListener("click", trocarImg)
+}
+
+
+ galeriaImg.forEach(clicarIamgem)
