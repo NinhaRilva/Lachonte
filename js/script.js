5fdb380d5bda02e423ea9f1dec6b002a4b9c84e2
Adriana R Silva <adrianarafael2010@gmail.com>
2023-05-02 07:19:38 -0300

    pagina cardapio e ajuste na pag login e promoção

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const galeriaImg = document.querySelectorAll
   (".galeria li img");
 
-
 function trocarImg(evento) {
   const imgPrincipal = document.querySelector(".img-principal-galeria img");
   const pegarImg = evento.currentTarget;
@@ -12,6 +11,4 @@ function clicarIamgem(imagem) {
   
   imagem.addEventListener("click", trocarImg)
 }
-
-
  galeriaImg.forEach(clicarIamgem)
